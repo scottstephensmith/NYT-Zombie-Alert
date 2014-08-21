d29f6c9f6b33a33cb458d4d1bd30d963108911c2
Scott Smith <scottstephensmith@gmail.com>
2014-08-21 13:00:32 -0400

    date parsing complete

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
+
 $(document).ready(function() {
+
     /*--- Display information modal box ---*/
     $(".what").click(function(){
         $(".overlay").fadeIn(1000);
@@ -10,6 +12,13 @@ $(document).ready(function() {
         $(".overlay").fadeOut(1000);
     });
 
+    //GETTING DATE FOR SEARCH (uses date.js library)
+    var today = Date.today().toString("yyyyMMdd");
+    var lastWeek = Date.today().addWeeks(-1).toString("yyyyMMdd");
+
+    console.log("today is " + today);
+    console.log("7 days before that was " + lastWeek);
+
 
     // THIS FUNCTION CALLS OUT TO THE NYT API, GETS DATA
     var findSearch = function(search){
@@ -20,7 +29,7 @@ $(document).ready(function() {
                         sort: 'creation'};
 
         var result = $.ajax({
-            url: 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=zombie&begin_date=20140101&end_date=20140107&sort=newest&api-key=e2ccb8664b26fad01845f1c1549fb8ac:16:69681960',
+            url: 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=zombie&begin_date=' + lastWeek + '&end_date=' + today + '&sort=newest&api-key=e2ccb8664b26fad01845f1c1549fb8ac:16:69681960',
             type: 'GET',
             dataType: 'json',
             data: search
@@ -29,6 +38,10 @@ $(document).ready(function() {
             console.log("success");
             console.log(result.response);
             console.log(result.response["docs"].length);
+
+            var articleCount = result.response["docs"].length;
+            console.log(articleCount);
+            $('#article-count').html("There have been " + articleCount + " articles mentioning zombies in the past 7 days.");
         })
         .fail(function(result) {
             console.log("error");
